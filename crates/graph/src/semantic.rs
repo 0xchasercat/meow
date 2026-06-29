3870c3736c7acd3472daca953d0803402a5ea6e2
mx <spuncheshirecat@gmail.com>
2026-06-30 00:29:23 +0700

    graph: adapt oxc diagnostics return fields

--- crates/graph/src/semantic.rs
+++ crates/graph/src/semantic.rs
@@ -36,7 +36,7 @@ impl SemanticGraph {
             let ret = SemanticBuilder::new().build(cst.program());
             SemDep {
                 semantic: ret.semantic,
-                errors: ret.errors,
+                errors: ret.diagnostics,
             }
         });
         Self { cell }
